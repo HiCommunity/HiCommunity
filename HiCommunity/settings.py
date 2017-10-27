a7333ca285f16b01a2b7631568bce4d5b2accfdd
Oliver <1225191678@qq.com>
2017-10-27 23:34:50 +0800

    字体css文件等本地化

--- HiCommunity/settings.py
+++ HiCommunity/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-     ('css', os.path.join(STATIC_ROOT, 'css')),
-     ('js', os.path.join(STATIC_ROOT, 'js')),
-     ('images', os.path.join(STATIC_ROOT, 'images')),
- )
+STATICFILES_DIRS = [
+    ('css', os.path.join(STATIC_ROOT, 'css')),
+    ('js', os.path.join(STATIC_ROOT, 'js')),
+    ('images', os.path.join(STATIC_ROOT, 'images')),
+]
