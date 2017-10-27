581ed6f3637744728869146b738983dbab535841
Oliver <1225191678@qq.com>
2017-10-27 18:30:42 +0800

    添加回答评论，未完成

--- static/js/questions/detail.js
+++ static/js/questions/detail.js
@@ -2,13 +2,28 @@
  * Created by Oliver on 2017/10/25 0025.
  */
 
+// bind submit comment
+function bindSubmitComment(form) {
+    var $form = getJQueryObject(form);
+    $form.submit(function (e) {
+        var $this = $(this);
+        var $textArea = $this.find('textarea');
+        if (!$textArea.val()) {
+            $textArea.focus();
+            return false;
+        }
+        e.preventDefault();
+    });
+}
+
+
 // scroll spy
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   });
 
-// submit answer
 $(function () {
+    // submit answer
     $('#new-answer-form').submit(function (e) {
         var $this = $(this);
         var $newAnswerContent = $('#new-answer-content');
@@ -39,6 +54,41 @@ $(function () {
                 removeLoadingCover();
             }
         });
-
     });
+
+    // show comment dialog
+    $('.show-comment').click(function (e) {
+        var $this = $(this);
+        var $section = $this.parent().parent();
+        var answerId = $section.attr('id');
+
+        var textArea = '<div class="row comment-wrapper">\n' +
+            '  <form class="col s12 comment-form">\n' +
+            '    <div class="row">\n' +
+            '      <div class="input-field col s12">\n' +
+            '        <textarea id="textarea-' + answerId + '" class="materialize-textarea" minlength="10"></textarea>\n' +
+            '        <label for="textarea-' + answerId + '" class="">我的看法</label>\n' +
+            '      </div>\n' +
+            '    </div>\n' +
+            '    <button class="btn waves-effect waves-light" type="submit" name="action">评论</button>\n' +
+            '  </form>\n' +
+            '</div>';
+
+        var $commentWrapper = $section.find('.comment-wrapper');
+
+        if ($this.hasClass('rotate-0')) {
+            $this.removeClass('rotate-0').addClass('rotate-90');
+        } else if ($this.hasClass('rotate-90')) {
+            $this.removeClass('rotate-90').addClass('rotate-0');
+        } else {
+            $this.addClass('rotate-90');
+        }
+        if ($commentWrapper.length > 0) {
+            $commentWrapper.slideToggle();
+        } else {
+            $(textArea).hide().appendTo($section).slideDown();
+        }
+    });
+
+
 });
